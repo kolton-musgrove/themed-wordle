f84924932be85c9f7eadbb7a4c3b52faff2b9c87
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-01-26 10:43:08 -0500

    initial wordle logic

--- src/components/wordle.js
+++ src/components/wordle.js
@@ -1,7 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function Wordle() {
 	return (
-		<h1>Wordle</h1>
+		<>
+			<h1>Wordle</h1>
+			<EntryLine />
+		</>
 	)
+}
+
+function EntryLine() {
+	return (
+		<form onSubmit={CheckEntry}>
+			<textarea value={ } />
+		</form>
+	)
+}
+
+function CheckEntry(event) {
+	console.log("An entry was submitted " + this.state.value)
 }
