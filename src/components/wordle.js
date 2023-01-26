92123badd7aa967838597d4af73469a2d46f681d
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-01-26 12:04:39 -0500

    inital entry acceptance logic

--- src/components/wordle.js
+++ src/components/wordle.js
@@ -5,18 +5,35 @@ export default function Wordle() {
 		<>
 			<h1>Wordle</h1>
 			<EntryLine />
+			<EntryLine />
+			<EntryLine />
+			<EntryLine />
+			<EntryLine />
 		</>
 	)
 }
 
 function EntryLine() {
+	const [entry, updateEntry] = useState("")
+
+
+	const handleInput = (e) => {
+		if (e.nativeEvent.inputType === 'insertLineBreak' && entry.length === 5) {
+			checkEntry()
+		}
+
+		if ((entry.length < 5 || (e.nativeEvent.inputType === 'deleteContentBackward')) && e.nativeEvent.inputType !== 'insertLineBreak') {
+			updateEntry(e.target.value)
+		}
+	}
+
+	const checkEntry = () => {
+		console.log("An entry was subitted: " + entry)
+	}
+
 	return (
-		<form onSubmit={CheckEntry}>
-			<textarea value={ } />
+		<form>
+			<textarea value={entry} onChange={e => handleInput(e)} />
 		</form>
 	)
-}
-
-function CheckEntry(event) {
-	console.log("An entry was submitted " + this.state.value)
 }
