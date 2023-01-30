2ed90d1f10d678cbec69f2058abfd09a6b9b9568
Kolton Musgrove <KoltonGMusgrove@gmail.com>
2023-01-30 09:05:10 -0500

    useEffect work

--- src/components/wordle.js
+++ src/components/wordle.js
@@ -1,39 +1,58 @@
-import React, { useState } from 'react'
+import React, { useEffect } from 'react'
+
+const wordOfTheDay = "hello"
+const acceptableCharacters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'Enter', 'Delete']
+
+const newGame = Array(6).fill(0).map(row => new Array(5).fill("O"))
 
 export default function Wordle() {
+	const wordIndex = 0
+	const characterIndex = 0
+	const board = newGame
+
+	const erase = () => {
+
+	}
+
+	const submit = () => {
+		if (characterIndex === 4) {
+
+		}
+	}
+
+	const enterCharacter = (code) => { }
+
+	useEffect(() => {
+		document.addEventListener("keydown", handleKeyDown)
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown)
+		}
+	})
+
+	const handleKeyDown = ({ code }) => {
+		if (acceptableCharacters.includes(code)) {
+			switch (code) {
+				case 'Enter': submit()
+				case 'Delete': erase()
+				default: enterCharacter(code)
+			}
+		}
+	}
+
 	return (
-		<>
+		<main>
 			<h1>Wordle</h1>
-			<EntryLine />
-			<EntryLine />
-			<EntryLine />
-			<EntryLine />
-			<EntryLine />
-		</>
-	)
-}
 
-function EntryLine() {
-	const [entry, updateEntry] = useState("")
-
-
-	const handleInput = (e) => {
-		if (e.nativeEvent.inputType === 'insertLineBreak' && entry.length === 5) {
-			checkEntry()
-		}
-
-		if ((entry.length < 5 || (e.nativeEvent.inputType === 'deleteContentBackward')) && e.nativeEvent.inputType !== 'insertLineBreak') {
-			updateEntry(e.target.value)
-		}
-	}
-
-	const checkEntry = () => {
-		console.log("An entry was subitted: " + entry)
-	}
-
-	return (
-		<form>
-			<textarea value={entry} onChange={e => handleInput(e)} />
-		</form>
+			{board.map((word, wordI) => {
+				return (
+					<div className='word'>{
+						word.map((char, charI) => {
+							return (<div className='letter'>{board[wordI][charI]}</div>)
+						})
+					}
+					</div>
+				)
+			})}
+		</main>
 	)
 }
